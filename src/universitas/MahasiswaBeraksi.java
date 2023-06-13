71b8525709fa194521ca2658262965365cd6e69e
MuhammadZainiAbdurrahman <ezennn17@gmail.com>
2023-06-13 10:48:51 +0800

    Menjalankan Program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -1,5 +1,19 @@
 package universitas;
 
 public class MahasiswaBeraksi {
-  
+    public static void main(String[] args) {
+        // Membuat objek Mahasiswa
+        Mahasiswa mahasiswa = new Mahasiswa("2110010432", "Muhammad Zaini Abdurrahman", "Teknik Informatika");
+
+        String detailMahasiswa = getDetailMahasiswa(mahasiswa);
+
+        System.out.println(detailMahasiswa);
+    }
+
+    public static String getDetailMahasiswa(Mahasiswa mahasiswa) {
+        String detail = "NPM: " + mahasiswa.getNpm()
+                + "\nNama: " + mahasiswa.getNama()
+                + "\nProgram Studi: " + mahasiswa.getProdi();
+        return detail;
+    }
 }
